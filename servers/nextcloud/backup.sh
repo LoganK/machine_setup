1806df79ef90ec4e9210d6e7e0bf25b964472112
Logan Kennelly <logan@logank.dev>
2023-03-19 15:43:39 -0700

    nextcloud: backup.sh: Use compose v2

--- servers/nextcloud/backup.sh
+++ servers/nextcloud/backup.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 set -e -u
 
-docker-compose exec -u www-data app php occ maintenance:mode --on
+docker compose exec -u www-data app php occ maintenance:mode --on
 
 export BACKUP_SKIP_VOLUME_RE=".*db[[:digit:]]+"
 ../../notes/backup_all_volumes.sh
 
 ../../notes/backup_postgres.sh
 
-docker-compose exec -u www-data app php occ maintenance:mode --off
+docker compose exec -u www-data app php occ maintenance:mode --off
 
